702633e4e5f66fee138db7d38aa35cb33b9d3692
Babatunde Mattieu <bbmattieu9@gmail.com>
2019-09-25 11:09:39 +0100

    testing the message service

---
+++ unit-testing/src/app/message.service.spec.ts
@@ -0,0 +1,36 @@
+import { MessageService } from './message.service';
+import { IterableDiffers } from '@angular/core';
+
+describe('MessageService', () => {
+  let service: MessageService;
+
+  beforeEach(() => {
+
+  });
+
+  it('should have no messages to start', () => {
+    service = new MessageService();
+
+    expect(service.messages.length).toBe(0);
+  });
+
+  it('should add a message when add is called', () => {
+    service = new MessageService();
+
+    service.add('new message added');
+
+    expect(service.messages.length).toBe(1);
+  });
+
+  it('should clear messages when clear is called', () => {
+      service = new MessageService();
+      service.add('message to be cleared!');
+
+      service.clear();
+
+      expect(service.messages.length).toBe(0);
+
+  });
+
+
+});
